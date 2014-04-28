a4bb09fcaf2bc2d97fe440e4e2dbf21a1d5485b4
Winters <luke.s.winters@gmail.com>
2014-04-28 11:09:36 -0700

    Simple page with button to initialize multiple pages

---
+++ pages.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Winters on 4/28/14.
+ */
+var data = {
+	"Gmail":"https://mail.google.com/mail/",
+	"Linkedin":"https://www.linkedin.com/",
+	"cnBeta":"http://www.cnbeta.com/",
+	"DigitalOcean":"https://cloud.digitalocean.com/",
+	"Weibo":"http://www.weibo.com/",
+	"Facebook":"https://www.facebook.com/",
+	"Renren":"http://www.renren.com/",
+	"Steam":"http://store.steampowered.com/"
+}
